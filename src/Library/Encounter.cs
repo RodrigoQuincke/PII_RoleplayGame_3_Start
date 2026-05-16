c21cc87486dfcd234d3af6f44cc92b86a9490875
Gerónimo Sosa Fried <GERONIMO.SOSA@correo.ucu.edu.uy>
2026-05-16 15:48:39 -0300

    Fixed Encounters

--- src/Library/Encounter.cs
+++ src/Library/Encounter.cs
@@ -1,26 +1,24 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Security.Principal;
-using Microsoft.Win32.SafeHandles;
 
 namespace Ucu.Poo.RolePlayGame
 {
-        
     public class Encounter
     {
-        List<Heroes> aliveHeroes = new List<Heroes>();
-        List<Enemies> aliveEnemies = new List<Enemies>();
         public void DoEncounter(List<Enemies> enemies, List<Heroes> heroes)
         {
+            List<Heroes> aliveHeroes = new List<Heroes>(heroes);
+            List<Enemies> aliveEnemies = new List<Enemies>(enemies);
+
             while (aliveHeroes.Count > 0 && aliveEnemies.Count > 0)
             {
-                EnemiesAttack(enemies, heroes);
-                foreach (Heroes hero in heroes)
+                for (int i = 0; i < aliveEnemies.Count; i++)
                 {
-                    if (hero.Health > 0)
-                        aliveHeroes.Add(hero);
+                    Heroes target = aliveHeroes[i % aliveHeroes.Count];
+                    target.ReceiveAttack(aliveEnemies[i]);
                 }
+
+                aliveHeroes = aliveHeroes.FindAll(h => h.Health > 0);
 
                 if (aliveHeroes.Count == 0)
                 {
@@ -28,56 +26,30 @@ namespace Ucu.Poo.RolePlayGame
                     break;
                 }
 
-                HeroesAttack(enemies, heroes);
-                foreach (Enemies enemy in enemies)
-                {
-                    if (enemy.Health > 0)
-                        aliveEnemies.Add(enemy);
-                }
-            }
-            if (aliveEnemies.Count == 0)
-            {
-                Console.WriteLine("Todos los enemigos han sido derrotados.");
-                return;
-            }
-        }
-        private List<Heroes> EnemiesAttack(List<Enemies> enemies, List<Heroes> heroes)
-        {
-            int i = 0;
-            foreach (Heroes hero in heroes)
-            {
-                hero.ReceiveAttack(enemies[i]);
-                i++;
-                if (i >= enemies.Count)
-                    i = 0;
-            }
-
-            return heroes;
-        }
-
-        private List<Enemies> HeroesAttack(List<Enemies> enemies, List<Heroes> heroes)
-        {
-            int i = 0;
-            foreach (Enemies enemy in enemies)
-            {
                 foreach (Heroes hero in aliveHeroes)
                 {
-                    enemy.ReceiveAttack(hero);
-                    DefeatEnemy(enemy, hero);
+                    foreach (Enemies enemy in aliveEnemies)
+                    {
+                        if (enemy.Health > 0)
+                        {
+                            int healthBefore = enemy.Health;
+                            enemy.ReceiveAttack(hero);
+                            if (healthBefore > 0 && enemy.Health <= 0)
+                            {
+                                Console.WriteLine($"{enemy.Name} ha sido derrotado por {hero.Name}.");
+                                hero.AddVictoryPoints(enemy);
+                                if (hero.AccumulatedVictoryPoints >= 5)
+                                    hero.Cure();
+                            }
+                        }
+                    }
                 }
-            }
-            return enemies;
-        }
 
-        private void DefeatEnemy(Enemies enemy, Heroes hero)
-        {
-            if (enemy.Health <= 0)
-            {
-                Console.WriteLine($"{enemy.Name} ha sido derrotado por {hero.Name}.");
-                hero.AddVictoryPoints(enemy);
-                if (hero.AccumulatedVictoryPoints >= 5)
-                    hero.Cure();
+                aliveEnemies = aliveEnemies.FindAll(e => e.Health > 0);
             }
+
+            if (aliveEnemies.Count == 0)
+                Console.WriteLine("Todos los enemigos han sido derrotados.");
         }
     }
 }
